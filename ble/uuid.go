43f68069e32cd9c4ec04909637360f4364efee92
Satoshi Konno <skonno@cybergarage.org>
2025-09-09 00:14:59 +0900

    feat(uuid): add MustUUIDFromUUIDString()

--- ble/uuid.go
+++ ble/uuid.go
@@ -26,6 +26,11 @@ func NewUUIDFromUUIDString(s string) (UUID, error) {
 	return types.NewUUIDFromUUIDString(s)
 }
 
+// MustUUIDFromUUIDString creates a UUID from a string representation and return base UUID if it fails.
+func MustUUIDFromUUIDString(s string) UUID {
+	return types.MustUUIDFromUUIDString(s)
+}
+
 // NewUUIDFromUUID16 creates a new UUID from the given 16-bit UUID.
 func NewUUIDFromUUID16(u uint16) UUID {
 	return types.NewUUIDFromUUID16(u)
